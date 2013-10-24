40e716c76d456f11c9c6e67c29748668b682d7e3
Dan Kolz <dan@emergentideas.com>
2013-10-24 12:26:11 -0500

    queue

--- resources/js/comet-msg-queue.jquery.js
+++ resources/js/comet-msg-queue.jquery.js
@@ -1,4 +1,22 @@
 (function() {
+	var getCookie = function(c_name) {
+		var c_value = document.cookie;
+		var c_start = c_value.indexOf(" " + c_name + "=");
+		if (c_start == -1) {
+			c_start = c_value.indexOf(c_name + "=");
+		}
+		if (c_start == -1) {
+			c_value = null;
+		} else {
+			c_start = c_value.indexOf("=", c_start) + 1;
+			var c_end = c_value.indexOf(";", c_start);
+			if (c_end == -1) {
+				c_end = c_value.length;
+			}
+			c_value = unescape(c_value.substring(c_start, c_end));
+		}
+		return c_value;
+	}
 	var genId = function(idLength) {
 		var uid = 'id';
 		while(idLength > 0) {
@@ -6,7 +24,7 @@
 			uid += digit;
 			idLength--;
 		}
-		return uid;
+		return getCookie('JSESSIONID') + uid;
 	}; 
 CometMsgQueue = {
 		delay: 10,
